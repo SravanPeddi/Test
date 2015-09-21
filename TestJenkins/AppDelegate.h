9f5d575bb276a9378e3f13b8c552806867d7a434
Sravan Peddi <sravanpeddi@Sravan-MacBook-Pro.local>
2015-09-21 16:26:52 +0530

    Initial Commit

---
+++ TestJenkins/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestJenkins
+//
+//  Created by Sravan Peddi on 9/21/15.
+//  Copyright (c) 2015 Sravan Peddi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestJenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestJenkins
+//
+//  Created by Sravan Peddi on 9/21/15.
+//  Copyright (c) 2015 Sravan Peddi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
